4f6373f0f7f3fae978cc2ce5bbd39f63be3cf606
Lionel <fullstackbeaver@gmail.com>
2022-02-01 19:33:00 +0100

    fix: fix error due OC's interface changes

--- src/extractor_start.js
+++ src/extractor_start.js
@@ -1,1 +1,1 @@
-var extractor = new Extractor("#mainContent section li", "#mainContent button.webapp-0-MuiButton-text");
+var extractor = new Extractor("#mainContent section li", ".MuiButton-root");
